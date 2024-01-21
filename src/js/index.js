997bd56af647b8b8ea73894b3912c712c6b4bf3c
Ivan-ISS <vanya5@inbox.ru>
2024-01-22 01:45:58 +0500

    project_cosmoscow: added script JS (change pictures by click and hover)

---
+++ src/js/index.js
@@ -0,0 +1,74 @@
+const btnPlus = document.getElementsByClassName('btn-plus');
+const btnMore = document.getElementsByClassName('btn-more');
+
+console.log(btnMore)
+
+let btnPlusDisplay = 'true';
+
+//-------------------------------------------------------------------------------------
+//--------------------------ФУНКЦИЯ ДЛЯ СМЕНЫ КАРТИНКИ ПО КЛИКУ МЫШКИ------------------
+
+function changePictureClick() {
+    
+        for (let i = 0; i < btnPlus.length; i++) {
+            btnPlus[i].addEventListener('click', (event) => {
+                /* console.log(event.currentTarget);
+                console.log(event.currentTarget.nextElementSibling);
+                console.log(event.currentTarget.nextElementSibling.firstElementChild);
+                console.log(event.currentTarget.nextElementSibling.lastElementChild); */
+
+                let screenWidth = document.documentElement.clientWidth;
+                if (screenWidth < 1280) {
+                    if (btnPlusDisplay === 'true') {
+                        event.currentTarget.nextElementSibling.firstElementChild.style.display = 'none';
+                        event.currentTarget.nextElementSibling.lastElementChild.style.display = 'block';
+                        event.currentTarget.firstElementChild.style.display = 'none';
+                        event.currentTarget.lastElementChild.style.display = 'block';
+                        btnPlusDisplay = 'false';
+                    } else {
+                        event.currentTarget.nextElementSibling.firstElementChild.style.display = 'block';
+                        event.currentTarget.nextElementSibling.lastElementChild.style.display = 'none';
+                        event.currentTarget.firstElementChild.style.display = 'block';
+                        event.currentTarget.lastElementChild.style.display = 'none';
+                        btnPlusDisplay = 'true';
+                    }
+                }
+                /* console.log(btnPlusDisplay); */
+                /* console.log(event.currentTarget.nextElementSibling.firstElementChild.style.display); */
+            })
+        }
+}
+
+//----------------------------------------------------------------------------------------
+//-------------ФУНКЦИЯ ДЛЯ СМЕНЫ КАРТИНКИ ПО НАВЕДЕНИЮ И УХОДУ КУРСОРА МЫШКИ--------------
+
+function changePictureHover() {
+    
+    for (let i = 0; i < btnPlus.length; i++) {
+        btnPlus[i].addEventListener('mouseover', (event) => {
+
+            let screenWidth = document.documentElement.clientWidth
+            if (screenWidth >= 1280) {
+                event.currentTarget.nextElementSibling.firstElementChild.style.display = 'none';
+                event.currentTarget.nextElementSibling.lastElementChild.style.display = 'block';
+                event.currentTarget.firstElementChild.style.display = 'none';
+                event.currentTarget.lastElementChild.style.display = 'block';
+            }
+        })
+
+        btnPlus[i].addEventListener('mouseout', (event) => {
+
+            let screenWidth = document.documentElement.clientWidth;
+            if (screenWidth >= 1280) {
+                event.currentTarget.nextElementSibling.firstElementChild.style.display = 'block';
+                event.currentTarget.nextElementSibling.lastElementChild.style.display = 'none';
+                event.currentTarget.firstElementChild.style.display = 'block';
+                event.currentTarget.lastElementChild.style.display = 'none';
+            }
+        })
+    }
+}
+
+//--------------------ВЫЗОВ ФУНКЦИЙ-----------------------
+changePictureHover();
+changePictureClick();
